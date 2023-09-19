fa22207e9bf97f4e8dbe4f28c4b4db28c9b8f921
lilliepalumbo <lillie.palumbo@mymail.champlain.edu>
2023-09-19 13:48:59 -0400

    Hex rules

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -4,8 +4,45 @@
 
 #include "HexagonGameOfLife.h"
 void HexagonGameOfLife::Step(World& world) {
+  for (int x = 0; x < world.SideSize(); x++) {
+    for (int y = 0; y < world.SideSize(); y++) {
+      int neighborCount = CountNeighbors(world, Point2D(x, y));
+      if (world.Get(Point2D(x, y)))
+      {
+        if (neighborCount < 2 || neighborCount > 3)
+        {
+          world.SetNext(Point2D(x, y), false);
+        }
+      }
+      else
+      {
+        if (neighborCount == 3)
+        {
+          world.SetNext(Point2D(x, y), true);
+        }
 
+      }
+    }
+  }
 }
 int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
-  return 0;
+  int neighborCount = 0;
+
+  world.Get(point.Left()) ? neighborCount++ : NULL;
+  world.Get(point.Right()) ? neighborCount++ : NULL;
+  world.Get(point.Up()) ? neighborCount++ : NULL;
+  world.Get(point.Down()) ? neighborCount++ : NULL;
+
+  if(point.y % 2 == 1)
+  {
+    world.Get(point + Point2D(1, 1)) ? neighborCount++ : NULL;
+    world.Get(point + Point2D(1, -1)) ? neighborCount++ : NULL;
+  }
+  else
+  {
+    world.Get(point + Point2D(-1, -1)) ? neighborCount++ : NULL;
+    world.Get(point + Point2D(-1, 1)) ? neighborCount++ : NULL;
+  }
+
+  return neighborCount;
 }
