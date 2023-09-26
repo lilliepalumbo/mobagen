9ab6466bc8d5cf70f1cb08aeb5f1cebe5a03d290
lilliepalumbo <lillie.palumbo@mymail.champlain.edu>
2023-09-26 13:54:45 -0400

    Fix and finish GameOfLife

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -5,24 +5,26 @@
 #include "HexagonGameOfLife.h"
 void HexagonGameOfLife::Step(World& world) {
   for (int x = 0; x < world.SideSize(); x++) {
-    for (int y = 0; y < world.SideSize(); y++) {
-      int neighborCount = CountNeighbors(world, Point2D(x, y));
-      if (world.Get(Point2D(x, y)))
-      {
-        if (neighborCount < 2 || neighborCount > 3)
-        {
-          world.SetNext(Point2D(x, y), false);
-        }
-      }
-      else
-      {
-        if (neighborCount == 3)
-        {
-          world.SetNext(Point2D(x, y), true);
-        }
+      for (int y = 0; y < world.SideSize(); y++) {
+          int neighborCount = CountNeighbors(world, {x, y});
+          bool isAlive = world.Get({x, y});
+          world.SetNext({x,y}, isAlive);
+          if (isAlive)
+          {
+              if (neighborCount < 2 || neighborCount > 3)
+              {
+                world.SetNext({x,y}, false);
+              }
+          }
+          else
+          {
+              if (neighborCount == 3)
+              {
+                world.SetNext({x,y}, true);
+              }
 
+          }
       }
-    }
   }
 }
 int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,39 +3,38 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
     for (int x = 0; x < world.SideSize(); x++) {
-      for (int y = 0; y < world.SideSize(); y++) {
-        int neighborCount = CountNeighbors(world, Point2D(x, y));
-        if (world.Get(Point2D(x, y)))
-        {
-          if(neighborCount < 2 || neighborCount > 3)
-          {
-            world.SetNext(Point2D(x, y), false);
-          }
-        }
-        else
-        {
-          if(neighborCount == 3)
-          {
-            world.SetNext(Point2D(x, y), true);
-          }
-        }
+        for (int y = 0; y < world.SideSize(); y++) {
+            int neighborCount = CountNeighbors(world, {x,y});
+            bool isAlive = world.Get({x,y});
+            world.SetNext({x,y}, isAlive);
 
-      }
+            if (isAlive)
+            {
+                if(neighborCount < 2 || neighborCount > 3)
+                {
+                  world.SetNext({x,y}, false);
+                }
+            }
+            else
+            {
+                if(neighborCount == 3)
+                {
+                  world.SetNext({x,y}, true);
+                }
+            }
+
+        }
     }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-    int neighborCount = 0;
-
-    world.Get(point.Up()) ? neighborCount++ : NULL;
-    world.Get(point.Left()) ? neighborCount++ : NULL;
-    world.Get(point.Down()) ? neighborCount++ : NULL;
-    world.Get(point.Right()) ? neighborCount++ : NULL;
-
-    world.Get(point.Up() + Point2D(1, 0)) ? neighborCount++ : NULL; // Upper Right
-    world.Get(point.Down() + Point2D(1, 0)) ? neighborCount++ : NULL; // Lower Right
-    world.Get(point.Up() - Point2D(1, 0)) ? neighborCount++ : NULL; // Upper Left
-    world.Get(point.Down() - Point2D(1, 0)) ? neighborCount++ : NULL; // Lower Left
-
-    return neighborCount;
+    return
+        world.Get(point.Up()) +
+        world.Get(point.Left()) +
+        world.Get(point.Down()) +
+        world.Get(point.Right()) +
+        world.Get(point.Up().Left()) +
+        world.Get(point.Down().Left()) +
+        world.Get(point.Up().Right()) +
+        world.Get(point.Down().Right());
 }
